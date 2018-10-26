87bff42a090b9318b6aa1a66b3d08a1647edd8a5
vadacpp <gtdsocial@icloud.com>
2018-10-27 00:31:33 +0300

    i3 complete maybe

--- i3.nix
+++ i3.nix
@@ -3,13 +3,22 @@ let
   fg = "#ffffff";
   bg = "#000000";
   mod = "Mod1";
+
   palette = (background: border: text: childBorder: indicator: {inherit background border text childBorder indicator;});
   keys = (commands: builtins.listToAttrs 
     (map (x: {name = "${mod}+${x}"; value = (builtins.getAttr x commands);}) 
     (builtins.attrNames commands))); # thanks balsoft
+
+  ws = (w: builtins.listToAttrs 
+    ((map (x: {name = x; value = "workspace ${builtins.getAttr x w}";}) (builtins.attrNames w)) ++ 
+     (map (x: {name = "Shift+${x}"; value = "move container to workspace ${builtins.getAttr x w}";}) (builtins.attrNames w))));  
+
+  countWS = 10; 
+  generatedWS = builtins.listToAttrs (map (x: {name = toString x; value = toString x;}) (builtins.genList (x: x) countWS));
 in {
   enable = true;
   config = {
+    fonts = [ "Terminus 8" ];
     /*
     colors = {
       background = bg;
@@ -18,22 +27,46 @@ in {
     };
     */
     
-    keybindings = keys {
+    keybindings = keys ({
       "h" = "focus left";
       "j" = "focus down";
       "k" = "focus up";
       "l" = "focus right";
 
-      "Shift + h" = "focus left";
-      "Shift + j" = "focus down";
-      "Shift + k" = "focus up";
-      "Shift + l" = "focus right";
+      "Shift+h" = "focus left";
+      "Shift+j" = "focus down";
+      "Shift+k" = "focus up";
+      "Shift+l" = "focus right";
 
-      "q" = "kill";
+      "v" = "split v";
+      "Shift+v" = "split h";
+
       "f" = "fullscreen toggle";
+      "r" = "mode resize";
+      "Shift+r" = "restart";
+      "q" = "kill";
+      "space" = "mode_toggle";
 
       "b" = "exec ${pkgs.chromium}/bin/chromium";
+      "d" = "exec ${pkgs.dmenu}/bin/dmenu_run";
       "Return" = "exec ${pkgs.xterm}/bin/xterm";
+    } // ws (generatedWS // {"0" = "10";})); # generate workspace
+
+    modes = {
+      resize = {
+        "h" = "resize shrink width 10 px or 10 ppt";
+        "j" = "resize shrink height 10 px or 10 ppt";
+        "k" = "resize grow height 10 px or 10 ppt";
+        "l" = "resize grow width 10 px or 10 ppt";
+        "Escape" = "mode default";
+        "Return" = "mode default";
+        "${mod}" = "mode default";
+      };
     };
+
+    bars = [{
+      "mode" = "dock";
+      "position" = "top";
+    }];
   };
 } 
