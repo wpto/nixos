314596d95b79bedfaf013e5d3a5b5e5b33c27345
ki <gtdsocial@icloud.com>
2019-11-20 21:43:53 +0300

    after a while...

--- configuration.nix
+++ configuration.nix
@@ -1,157 +1,120 @@
-#q Edit this configuration file to define what should be installed on
+# Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
+
 {
-  nixpkgs.config.allowUnfree = true;
-  imports = [ ./hardware-configuration.nix ];
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-label/nixos";
-    fsType = "ext4";      
-  };
+  # boot
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "/dev/sda"; 
+  boot.loader.grub.useOSProber = true;
 
-  swapDevices = [ { device = "/dev/disk/by-label/swap"; } ];
-
-  # Use the GRUB 2 boot loader.
-  boot = {
-    loader.grub = {
-      enable = true;
-      version = 2;
-      device = "/dev/sda"; # or "nodev" for efi only
-      useOSProber = true;
-    };
-    
-    kernel.sysctl = {
-      "net.ipv4.ip_default_ttl" = 65;
-    };
-  };
+  # networking
   networking.hostName = "nixos";
-  networking.wireless = {
-    enable = true;
-    networks = {
-      "Lesya_WiFi".pskRaw = "cfabe4cbc6473fc678e5aba10c67033cbbd7e518245fd40f650a87765be3de65";
-      "Thunderbird".pskRaw = "d10ff16e6d23e42958e9f76af1369970c49ea1077d55b3ad78e78e3318366fc6";
-      "RT-WiFi_574D".pskRaw = "1195d1f7509bebb537886f0592812b6ba4a1dbd6c2856c9182bf65829db5a612";
-      "10101" = {
-         pskRaw = "5767794e8c48d7fee755d7051e7e1d0173d55890d30bb61f12da50c051c5a02f";
-         hidden = true;
-      };
-    };
-  };
+  networking.useDHCP = false;
+  networking.interfaces.enp2s0.useDHCP = true;
+  networking.networkmanager.enable = true;
 
+  # i18n
   i18n = {
     consoleFont = "Lat2-Terminus16";
     consoleKeyMap = "us";
     defaultLocale = "en_US.UTF-8";
   };
 
+  # time
   time.timeZone = "Europe/Moscow";
 
+  # nixpkgs
+  nixpkgs.config.allowUnfree = true;
+
+  # environment
   environment.systemPackages = with pkgs; [
-    wget vim firefox zathura tdesktop pavucontrol milkytracker
+    wget vim firefox termite networkmanagerapplet
   ];
 
-
-
-  # Enable sound.
+  # sound
   sound.enable = true;
   hardware.pulseaudio.enable = true;
 
-# Enable the X11 windowing system.
+
+  # xserver
   services.xserver = {
     enable = true;
-    synaptics.enable = true;
+    layout = "us,ru";
+    xkbOptions = "grp:win_space_toggle,grp_led:scroll,caps:swapescape";
+    
+    desktopManager.xterm.enable = false;
 
     displayManager.auto = {
       enable = true;
-      user = "nixuser";
+      user = "ki";
     };
-
-    desktopManager.xterm.enable = false;
 
     windowManager = {
       default = "i3";
       i3.enable = true;
+      i3.configFile = pkgs.writeText "i3-config-file" (import ./i3.nix pkgs); 
+      #i3.package = let
+      #  text = import ./i3.nix pkgs;
+      #  configFile = pkgs.writeText "i3-config-file" text;
+      #in pkgs.writeScriptBin "i3" ''
+      #  #!${pkgs.stdenv.shell}
+      #  exec ${pkgs.i3} -c ${configFile}
+      #'';
     };
-      
-    layout = "us,ru";
-    xkbVariant = "colemak,";
-    xkbOptions = "grp:alt_shift_toggle,grp_led:scroll,caps:swapescape";
 
-    videoDrivers = ["intel"];
-  };
-
-  
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    extraPackages = with pkgs; [ xorg.xf86videointel ];
-  };
-
-  fonts = {
-    enableDefaultFonts = true;
-    fonts = with pkgs; [
-      terminus_font
-    # inconsolata
-    # libertine
-    # source-code-pro
-    # source-sans-pro
-    # source-serif-pro
-    ];
-    fontconfig.defaultFonts = {
-      monospace = [ "terminus_font" ];
-      sansSerif = [ "FreeSans" ];
-      serif     = [ "FreeSerif" ];  
-    };
+    videoDrivers = [ "nvidia" ];
   };
 
   services.compton = {
     enable = true;
-    backend = "glx"; #glx xrender xr_glx_hybrid
-    refreshRate = 60;
-    vSync = "opengl-swc"; # opengl-swc drm_slow opengl_slow opengl-oml_slow opengl-mswc_strange
+
+    backend = "xrender";
+    vSync = true; 
   };
 
-  users.mutableUsers = false;
+  programs.fish.enable = true;
 
-  users.extraUsers.nixuser = {
-    isNormalUser = true;
-    description  = "";
-    password     = ""; 
-    uid = 1000;
+  # users
+  users = {
+    mutableUsers = false;
+
+    users.root = {
+      password = "";
+      shell = pkgs.fish;
+    };
+
+    users.ki = {
+      isNormalUser = true;
+      password = "";
+      uid = 1000;
+      extraGroups = [ "networkmanager" ];
+      shell = pkgs.fish;
+    };
   };
 
-  users.users.root = {
-    password = "";
-  };
-
-  services.tor = {
-    enable = true;
-    client.enable = true;
-    client.privoxy.enable = true;
-    torsocks.enable = true;
-  };
-
+  # sudo
   security.sudo = {
     enable = true;
     extraRules = [{
+      groups = [ "users" ];
       commands = [{
         command = "ALL";
         options = [ "NOPASSWD" ];
       }];
-      groups   = [ "users" ];
     }];
   };
 
-  services.openssh = {
-    enable = true;
-    listenAddresses = [ { addr = "0.0.0.0"; port = 22; } ];
-  };
-
-
-
-  system.stateVersion = "19.03";
+  # system
+  system.stateVersion = "19.09"; # Did you read the comment?
 
 }
+
--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -8,11 +8,27 @@
     [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
     ];
 
-  boot.initrd.availableKernelModules = [ "ehci_pci" "ata_piix" "usb_storage" "usbhid" "sd_mod" "sr_mod" ];
+  boot.initrd.availableKernelModules = [ "ahci" "ohci_pci" "ehci_pci" "pata_atiixp" "usb_storage" "usbhid" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
-  nix.maxJobs = lib.mkDefault 4;
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/nixos";
+      fsType = "ext4";
+    };
+
+    "/home" = {
+      device = "/dev/disk/by-label/home";
+      fsType = "ext4";
+    };
+  };
+     
+
+  swapDevices =
+    [ { device = "/dev/disk/by-label/swap"; }
+    ];
+
+  nix.maxJobs = lib.mkDefault 6;
 }
